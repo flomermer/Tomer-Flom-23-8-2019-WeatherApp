f884531628c621a506c23ecd8824640fcd42bf39
flomermer <flom.tomer@gmail.com>
2019-08-24 20:23:31 +0300

    prod 1.1

--- src/misc/helper.js
+++ src/misc/helper.js
@@ -1,6 +1,6 @@
 const getWeatherIconSrc = (iconNumber) => {
   iconNumber = iconNumber<10 ? `0${iconNumber}` : iconNumber;
-  const src = `http://apidev.accuweather.com/developers/Media/Default/WeatherIcons/${iconNumber}-s.png`;
+  const src = `https://apidev.accuweather.com/developers/Media/Default/WeatherIcons/${iconNumber}-s.png`;
   return src;
 }
 
